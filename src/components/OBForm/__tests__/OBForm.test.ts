b326586a9a73a9dcf30bfc143d9245c8cf1d0921
Mohamed <justmoe23@gmail.com>
2023-11-30 11:26:10 -0500

    remove unused css

---
+++ src/components/OBForm/__tests__/OBForm.test.ts
@@ -0,0 +1,8 @@
+// import { render } from "@testing-library/react";
+// import OBForm from "../OBForm";
+
+// describe("Testing the onboarding form", () => {
+//   it("should display an error if the corporation number is invalid", async () => {
+//     render(<OBForm />);
+//   });
+// });
